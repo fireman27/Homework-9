b33103848e7dff112682a3fa0fe21bf0dd36f67b
fireman <fireman2747@yandex.ru>
2022-11-16 13:39:12 +0800

    add Homework 001 show numbers m - n

---
+++ Homework001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+while(M <= N)
+{
+    Console.Write($"{M} ");
+    M = M + 1;
+}
