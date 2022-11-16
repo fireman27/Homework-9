3b156d116fa24106a6ec4d0d400dcaf2ef090a48
fireman <fireman2747@yandex.ru>
2022-11-16 14:53:20 +0800

    add Homework 002 sum nambers m - n

---
+++ Homework002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы.
+
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+int summ = 0;
+while (M <= N)
+{
+    summ = summ + M;  
+    M++;
+              
+}
+Console.WriteLine($"Сумма элементов от M до N равна {summ}");
+Console.WriteLine();
+   
+    
