0c980298904ebd9621478f8cd5e4f9b033d63be8
fireman <fireman2747@yandex.ru>
2022-11-17 16:21:30 +0800

    add Homework 001 сделал через рекурсию

--- Homework001/Program.cs
+++ Homework001/Program.cs
@@ -1,11 +1,15 @@
 ﻿// Показать натуральные числа от M до N, N и M заданы
+
+string ShowNumbers(int N, int M)
+{
+    if (N >= M) return ShowNumbers(N-1, M) + $"{N} ";
+    else return " ";
+}
+
+//Console.Clear();
 Console.WriteLine("Введите число M: ");
 int M = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Введите число N: ");
 int N = int.Parse(Console.ReadLine() ?? "0");
 
-while(M <= N)
-{
-    Console.Write($"{M} ");
-    M = M + 1;
-}
+Console.WriteLine(ShowNumbers(N, M));
