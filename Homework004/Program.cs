9363bc3b5cc6a927fa6bc52377333f2602062e8b
fireman <fireman2747@yandex.ru>
2022-11-17 17:44:24 +0800

    add Homework 004 degree number A

---
+++ Homework004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую стень B
+int DegreeNumber(int a, int b)
+{
+    if (b == 1) return a;
+    return a * DegreeNumber(a, b - 1);     
+}
+
+Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите степень числа А: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"Число А возведенное в степень B = {DegreeNumber(a,b)}");  
