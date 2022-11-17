0d275c4b51949876d19c3d4179c92ec6e53c2acd
fireman <fireman2747@yandex.ru>
2022-11-17 17:29:30 +0800

    add Homework 003 исправил ошибки

--- Homework003/Program.cs
+++ Homework003/Program.cs
@@ -1,20 +1,17 @@
 ﻿// Написать программу вычисления функции Аккермана
+int Akk(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else
+    if ((n != 0) && (m == 0)) return Akk(n - 1, 1);
+    else return Akk(n - 1, Akk(n, m - 1));
+}
+
 Console.WriteLine("Введите число M: ");
 int m = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Введите число N: ");
-int n = int.Parse(Console.ReadLine() ?? "0");
-
-int Akk(int n, int m)
-{
-    if (n == 0)
-        return m + 1;
-    else
-      if ((n != 0) && (m == 0))
-        return Akk(n - 1, 1);
-    else
-        return Akk(n - 1, Akk(n, m - 1));
-}
- 
+int n = int.Parse(Console.ReadLine() ?? "0"); 
+Console.WriteLine();
 Console.WriteLine(Akk(m, n)); 
 
 
