8097538540f3a2c9fa19068ddf9ff67b468de2d8
fireman <fireman2747@yandex.ru>
2022-11-17 21:55:03 +0800

    add Homework 002 Sum Numbers

--- Homework002/Program.cs
+++ Homework002/Program.cs
@@ -1,18 +1,16 @@
 ﻿// Найти сумму элементов от M до N, N и M заданы.
 
+int NumberSum(int M, int N)
+{
+    if (M < N) return M + NumberSum(M + 1, N);
+    return M;
+}
+
 Console.WriteLine("Введите число M: ");
 int M = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Введите число N: ");
 int N = int.Parse(Console.ReadLine() ?? "0");
 
-int summ = 0;
-while (M <= N)
-{
-    summ = summ + M;  
-    M++;
-              
-}
-Console.WriteLine($"Сумма элементов от M до N равна {summ}");
-Console.WriteLine();
-   
-    
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {NumberSum(M,N)}"); 
+
+
